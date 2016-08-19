48d3b75a93fa7d0fd1122b670b7ce677b61ef82a
itsMagondu <magondunjenga@gmail.com>
2016-08-19 08:45:37 +0300

    Fixing kalman filter for NN

--- kalmanfilter.py
+++ kalmanfilter.py
@@ -1,63 +1,84 @@
 # Kalman filter in Python adopted from http://scipy-cookbook.readthedocs.io/items/KalmanFiltering.html
-
 import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-plt.rcParams['figure.figsize'] = (10, 8)
+class KalmanFilter:
+	def __init__(self,base_value=24,iterations=50,initial_guess=20.0,posteri_estimate=4.0,plot=False):
+		# intial parameters
+		self.n_iter = iterations  # How many iterations to create test data
+		sz = (self.n_iter,) # size of array
+		self.x = base_value # This is the base value that shall be used to create noisy data. It is the true value
+		self.z = np.random.normal(self.x,1,size=sz) # observations (normal about x, sigma=0.1)
 
-# intial parameters
-n_iter = 50
-sz = (n_iter,) # size of array
-x = 24
-z = np.random.normal(x,1,size=sz) # observations (normal about x, sigma=0.1)
+		self.Q = 1e-5 # process variance
 
-Q = 1e-5 # process variance
+		# allocate space for arrays
+		self.xhat=np.zeros(sz)      # a posteri estimate of x
+		self.P=np.zeros(sz)         # a posteri error estimate
+		self.xhatminus=np.zeros(sz) # a priori estimate of x
+		self.Pminus=np.zeros(sz)   
 
-# allocate space for arrays
-xhat=np.zeros(sz)      # a posteri estimate of x
-P=np.zeros(sz)         # a posteri error estimate
-xhatminus=np.zeros(sz) # a priori estimate of x
-Pminus=np.zeros(sz)   
+		# a priori error estimate
+		self.K=np.zeros(sz)         # gain or blending factor
 
-# a priori error estimate
-K=np.zeros(sz)         # gain or blending factor
+		self.R = 2
 
-R = 2
+		# intial guesses
+		self.xhat[0] = initial_guess #Initial estimate
+		self.P[0] = posteri_estimate#Estimate of the error made
 
-# intial guesses
-xhat[0] = 20.0
-P[0] = 21.0
+		self.plot = plot
+		
+	def filter(self):
+		print  "starting the filter"
+		start = time.time()
+		for k in range(1,self.n_iter):
+		    # time update
+		    self.xhatminus[k] = self.xhat[k-1]
+		    self.Pminus[k] = self.P[k-1]+self.Q
 
-print  "starting the filter"
-start = time.time()
-for k in range(1,n_iter):
-    # time update
-    xhatminus[k] = xhat[k-1]
-    Pminus[k] = P[k-1]+Q
+		    # measurement update
+		    self.K[k] = self.Pminus[k]/( self.Pminus[k]+self.R )
+		    self.xhat[k] = self.xhatminus[k]+self.K[k]*(self.z[k]-self.xhatminus[k])
+		    self.P[k] = (1-self.K[k])*self.Pminus[k]
+		end = time.time()
 
-    # measurement update
-    K[k] = Pminus[k]/( Pminus[k]+R )
-    xhat[k] = xhatminus[k]+K[k]*(z[k]-xhatminus[k])
-    P[k] = (1-K[k])*Pminus[k]
-end = time.time()
+		print("Took %s seconds" % (time.time() - start))
 
-print("Took %s seconds" % (time.time() - start))
+		#print "Noisy data: "
+		#print self.z 
 
-plt.figure()
-plt.plot(z,'k+',label='noisy measurements')
-plt.plot(xhat,'b-',label='a posteri estimate')
-plt.axhline(x,color='g',label='truth value')
-plt.legend()
-plt.title('Estimate vs. iteration step', fontweight='bold')
-plt.xlabel('Iteration')
-plt.ylabel('Temperature')
+		#print "Estimates:"
+		#print self.xhat
 
-plt.figure()
-valid_iter = range(1,n_iter) # Pminus not valid at step 0
-plt.plot(valid_iter,Pminus[valid_iter],label='a priori error estimate')
-plt.title('Estimated $\it{\mathbf{a \ priori}}$ error vs. iteration step', fontweight='bold')
-plt.xlabel('Iteration')
-plt.ylabel('$(Temparature)^2$')
-plt.setp(plt.gca(),'ylim',[0,.01])
-plt.show()
+		#print "Truth Value:"
+		#print self.x
+
+		#print "Error estimate"
+		#print self.P
+
+		if self.plot:
+			self.plot_results()
+
+		return self.z, self.xhat, self.x
+
+	def plot_results(self):
+		plt.rcParams['figure.figsize'] = (10, 8)
+		plt.figure()
+		plt.plot(self.z,'k+',label='noisy measurements')
+		plt.plot(self.xhat,'b-',label='a posteri estimate')
+		plt.axhline(self.x,color='g',label='truth value')
+		plt.legend()
+		plt.title('Estimate vs. iteration step', fontweight='bold')
+		plt.xlabel('Iteration')
+		plt.ylabel('Temperature')
+
+		#plt.figure()
+		#valid_iter = range(1,self.n_iter) # Pminus not valid at step 0
+		#plt.plot(valid_iter,self.Pminus[valid_iter],label='a priori error estimate')
+		#plt.title('Estimated $\it{\mathbf{a \ priori}}$ error vs. iteration step', fontweight='bold')
+		#plt.xlabel('Iteration')
+		#plt.ylabel('$(Temparature)^2$')
+		#plt.setp(plt.gca(),'ylim',[0,.01])
+		plt.show()
